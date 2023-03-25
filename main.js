3ed4abd8b66b9cf4dfeaf72327bdce16e2c3c0cd
Younes Brahimi <43382860+younesbram@users.noreply.github.com>
2023-03-25 03:15:27 -0400

    Added some jokes and bugfix

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 const jokesList = document.getElementById("jokes-list");
 
 const jokes = [
-    {
-        caption: "Redesigning Netflix push notifications - 1 trillion users",
-        url: "https://raw.githubusercontent.com/younesbram/promptengineer/master/jokes/joke1.txt",
-    },
-    // Add more jokes here
+  {
+    caption: "Redesigning Netflix push notifications - 1 trillion users",
+    url: "https://raw.githubusercontent.com/younesbram/promptengineer/master/jokes/joke1.txt"
+  },
+  {
+    caption: "Skyrim Engineer",
+    url: "https://raw.githubusercontent.com/younesbram/promptengineer/master/jokes/joke2.txt"
+  },
+  {
+    caption: "Apple iSeinfeld",
+    url: "https://raw.githubusercontent.com/younesbram/promptengineer/master/jokes/joke3.txt"
+  }
+  // Add more jokes here
 ];
 
 jokes.forEach((joke) => {
-    const li = document.createElement("li");
-    li.className = "joke";
-    const a = document.createElement("a");
-    a.href = "#";
-    a.setAttribute("data-url", joke.url);
-    a.innerText = joke.caption;
-    li.appendChild(a);
-    jokesList.appendChild(li);
+  const li = document.createElement("li");
+  li.className = "joke";
+  const a = document.createElement("a");
+  a.href = "#";
+  a.setAttribute("data-url", joke.url);
+  a.innerText = joke.caption;
+  li.appendChild(a);
+  jokesList.appendChild(li);
 });
 
 jokesList.addEventListener("click", (event) => {
-    if (event.target.tagName === "A") {
-        event.preventDefault();
-        const url = event.target.getAttribute("data-url");
-        fetch(url)
-          .then((response) => response.text())
-          .then((joke) => {
-            window.location.href = `joke.html?joke=${encodeURIComponent(joke)}`;
-          });
-    }
+  if (event.target.tagName === "A") {
+    event.preventDefault();
+    const url = event.target.getAttribute("data-url");
+    fetch(url)
+      .then((response) => response.text())
+      .then((joke) => {
+        window.location.href = `joke.html?joke=${encodeURIComponent(joke)}`;
+      });
+  }
 });
